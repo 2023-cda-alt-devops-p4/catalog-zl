a024e79cba57674c0f95b5cbce9fdc34b8af036d
Zakkios <laskari.zakaria@gmail.com>
2023-09-23 11:08:40 +0200

    feat: conversion de la navbar en side menu hamburger
    - Ajout du useState isOpen
    - Ajout de nouveau composant avec styled component : NavbarContent,
      CollapseButton, QuitButton et QuitButtonContainer
    - Ajout du style et d'une animation pour l'apparition et la disparition
      du menu

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,34 +1,69 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
+import styled from "styled-components"
 
 const Navbar = () => {
-  return (
-    <nav>
-      <div>
-        <NavLink 
+  const [isOpen, setIsOpen] = useState(false)
 
+  return (
+    <>
+    <CollapseButton toggle={isOpen} onClick={() => setIsOpen(!isOpen)}>Btn</CollapseButton>
+    <NavbarContent toggle={isOpen} onClick={() => setIsOpen(!isOpen)}>
+      <QuitButtonContainer>
+        <QuitButton>Quit</QuitButton>
+      </QuitButtonContainer>
+        <NavLink 
           to={'/'} 
           className={({ isActive }) =>
           isActive ? "navlink active" : "navlink"
-          }>
+        }>
             Home
         </NavLink>
         <NavLink 
           to={'/uml'} 
           className={({ isActive }) =>
           isActive ? "navlink active" : "navlink"
-          }>
+        }>
             UML
         </NavLink>
         <NavLink 
           to={'/merise'} 
           className={({ isActive }) =>
           isActive ? "navlink active" : "navlink"
-          }>
+        }>
             Merise
         </NavLink>
-      </div>
-    </nav>
+    </NavbarContent>
+    </>
   )
 }
 
 export default Navbar
+
+const NavbarContent = styled.nav`
+transform: translateX(${({ toggle }) => (toggle ? "-250px" : "0")});
+position: fixed;
+height: 100vh;
+width: 250px;
+z-index: 2;
+display: flex;
+flex-direction: column;
+background-color: #333;
+transition: transform 0.3s ease-in-out;`
+
+const CollapseButton = styled.button`
+  background-color: ${({ toggle }) => (toggle ? "red" : "blue")};
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  z-index: 10;
+`;
+
+const QuitButton = styled.button`
+  positon: absolute;`
+
+const QuitButtonContainer = styled.div`
+  text-align: end;
+  background-color: #333;
+  justify-content: flex-end;
+`
