f60685f2da1a25ec2260eeff709b2c1dd9539e02
Zakkios <laskari.zakaria@gmail.com>
2023-09-26 15:25:18 +0200

    feat: Implémentation de UML et Merise dans la page Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,13 @@
-import { NavLink } from "react-router-dom"
-import Accordion from "../component/Accordion"
-import Breadcrumb from "../component/Breadcrumb"
-import uml from "../data/uml.json"
+import Merise from "./Merise"
+import Uml from "./Uml"
 
 const Home = () => {
 
   return (
-    <div className="container">
-      <Breadcrumb />
-      {uml.category.map((item, index) => {
-        return (
-          <>
-            <NavLink to={`/UML/${item.categoryName}`}>{item.categoryName}</NavLink>
-            <Accordion key={index} category={item.categoryName} data={item.diagrammes} />
-          </>
-        )
-      })}
-    </div>
+    <>
+      <Uml />
+      <Merise />
+    </>
   )
 }
 
