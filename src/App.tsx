86b34e3b95381ecade82586ecabeb4955f87e5b8
Zakkios <laskari.zakaria@gmail.com>
2023-09-28 14:43:26 +0200

    feat: mise en place des pages MeriseDiagram et Category
    - Mise en place de la logique de la page sur la base des pages UML

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './component/Navbar';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Uml from './pages/Uml';
-import Merise from './pages/Merise';
-import Banner from './component/Banner';
-import UmlCategory from './pages/UmlCategory';
-import UmlDiagram from './pages/UmlDiagram';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Uml from "./pages/Uml";
+import Merise from "./pages/Merise";
+import Banner from "./component/Banner";
+import UmlCategory from "./pages/UmlCategory";
+import UmlDiagram from "./pages/UmlDiagram";
+import MeriseCategory from "./pages/MeriseCategory";
+import MeriseDiagram from "./pages/MeriseDiagram";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Banner />
-    <Routes>
-      <Route path='*' element={<NotFound />} />
-      <Route path='/' element={<Home/>} />
-      <Route path='/UML/' element={<Uml />} />
-      <Route path='/UML/:category/:name' element={<UmlDiagram />} />
-      <Route path='/UML/:category/' element={<UmlCategory />} />
-      <Route path='/merise/' element={<Merise/>} />
-      <Route path='/Merise/:category/:name' element={<Merise/>} />
-      <Route path='/Merise/:category/' element={<Merise/>} />
-    </Routes>
+      <Navbar />
+      <Banner />
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/UML/" element={<Uml />} />
+        <Route path="/UML/:category/:name" element={<UmlDiagram />} />
+        <Route path="/UML/:category/" element={<UmlCategory />} />
+        <Route path="/merise/" element={<Merise />} />
+        <Route path="/Merise/:category/:name" element={<MeriseDiagram />} />
+        <Route path="/Merise/:category/" element={<MeriseCategory />} />
+      </Routes>
     </>
   );
 }
--- src/pages/MeriseCategory.tsx
+++ src/pages/MeriseCategory.tsx
@@ -1,8 +1,22 @@
+import { useParams } from "react-router-dom";
+import merise from "../data/merise.json";
+import Accordion from "../component/Accordion";
+import Breadcrumb from "../component/Breadcrumb";
 
 const MeriseCategory = () => {
-  return (
-    <div>MeriseCategory</div>
-  )
-}
+  const { category } = useParams();
+  const modelByCategory = merise.category.find(
+    (item) => item.categoryName === category
+  );
+  const diagrammes = modelByCategory?.diagrammes || [];
 
-export default MeriseCategory
+  return (
+    <div className="container">
+      <Breadcrumb />
+      <h1 className="title">{category}</h1>
+      <Accordion category={category} data={diagrammes} />
+    </div>
+  );
+};
+
+export default MeriseCategory;
