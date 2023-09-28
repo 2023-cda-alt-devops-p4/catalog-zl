7c25ce2974e099e4c829f502c08667117021dd42
Zakkios <laskari.zakaria@gmail.com>
2023-09-28 08:18:19 +0200

    feat: Ajout des diagrammes UML manquant et des images
    - Creation du dossier imgs
    - Remplissage du fichier uml.json et des images d'exemple
    - Integration des images dans la page UmlDiagram.tsx

--- src/pages/UmlDiagram.tsx
+++ src/pages/UmlDiagram.tsx
@@ -3,55 +3,65 @@ import uml from "../data/uml.json";
 import Breadcrumb from "../component/Breadcrumb";
 import styled from "styled-components";
 
-
 interface diagram {
-    name: string;
-    description: string;
-    exempleImage: string;
-    elements: string[];
+  name: string;
+  description: string;
+  exempleImage: string;
+  elements: string[];
 }
 
 const UmlDiagram = () => {
-    const { category, name } = useParams();
-    const umlByCategory = uml.category.find(item => item.categoryName === category);
-    const diagram : diagram | undefined = umlByCategory?.diagrammes.find(item => item.name === name);
-
+  const { category, name } = useParams();
+  const umlByCategory = uml.category.find(
+    (item) => item.categoryName === category
+  );
+  const diagram: diagram | undefined = umlByCategory?.diagrammes.find(
+    (item) => item.name === name
+  );
 
   return (
     <>
-        <Breadcrumb />
-        <div className="container">
-            <h1 className="title">{ category }</h1>
-            <h3 className="subtitle">{ name }</h3>
-            <p className="description">{ diagram?.description }</p>
-            <ExempleContainer>
-                <h3 className="subtitle">Exemple :</h3>
-                <div>
-                    <img src={diagram?.exempleImage} alt="" width={"100%"} />
-                </div>
-                <div>
-                    { diagram?.elements.map((item: string) => {
-                        return (
-                            <>
-                                <Elements key={crypto.randomUUID()}>{ item }</Elements><br />
-                            </>
-                            )
-                        }) }
-                </div>
-            </ExempleContainer>
-        </div>
+      <Breadcrumb />
+      <div className="container">
+        <h1 className="title">{category}</h1>
+        <h3 className="subtitle">{name}</h3>
+        <p className="description">{diagram?.description}</p>
+        <ExempleContainer>
+          <h3 className="subtitle">Exemple :</h3>
+          <ExemplePicture src={diagram?.exempleImage} alt="" />
+          <h3 className="subtitle">Éléments :</h3>
+          <div>
+            {diagram?.elements.map((item: string) => {
+              return (
+                <>
+                  <Elements key={crypto.randomUUID()}>
+                    <span className="bold">{item.split(":")[0]} :</span>
+                    {item.split(":")[1]}
+                  </Elements>
+                  <br />
+                </>
+              );
+            })}
+          </div>
+        </ExempleContainer>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default UmlDiagram
+export default UmlDiagram;
 
 const ExempleContainer = styled.div`
-display: flex;
-flex-direction: column;
-gap: 1em;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+`;
 
 const Elements = styled.div`
-    text-align: justify;
-`
+  text-align: justify;
+`;
+
+const ExemplePicture = styled.img`
+  max-width: 100%;
+  align-self: center;
+`;
