831e95b029de22236781a84c66845ba3d103c348
Zakkios <laskari.zakaria@gmail.com>
2023-09-28 14:21:39 +0200

    style: modification texte de presentation merise + ajout image de presentation

--- src/pages/Merise.tsx
+++ src/pages/Merise.tsx
@@ -13,16 +13,24 @@ const Merise = () => {
       <h1 className="title">
         Liste des differents modèles Merise classé par catégorie
       </h1>
-      <p className="description">
-        La méthodologie Merise est comme la carte de navigation pour les
-        concepteurs de systèmes d'information. Elle offre une approche
-        structurée pour analyser, concevoir et mettre en œuvre des solutions
-        informatiques. En utilisant des modèles conceptuels, organisationnels,
-        logiques et physiques, Merise aide les équipes de développement à créer
-        des systèmes robustes et efficaces. Explorez ci-dessous les différents
-        modèles Merise et leur rôle dans le développement de solutions
-        informatiques.
-      </p>
+      <div className="presentation">
+        <p className="description">
+          La méthodologie Merise est comme la carte de navigation pour les
+          concepteurs de systèmes d'information. Elle offre une approche
+          structurée pour analyser, concevoir et mettre en œuvre des solutions
+          informatiques. En utilisant des modèles conceptuels, logiques et
+          physiques, Merise aide les équipes de développement à créer des
+          systèmes robustes et efficaces. Ces trois niveaux d'abstraction, à
+          savoir les modèles conceptuels, logiques et physiques, permettent aux
+          équipes de développement de concevoir des systèmes informatiques qui
+          répondent aux besoins de l'organisation, en commençant par une vision
+          globale et en descendant progressivement vers une spécification
+          détaillée, garantissant ainsi la robustesse et l'efficacité des
+          solutions mises en œuvre. Explorez ci-dessous les différents modèles
+          Merise et leur rôle dans le développement de solutions informatiques.
+        </p>
+        <img src="./imgs/merise.png" alt="" />
+      </div>
       {merise.category.map((item, index) => {
         return (
           <>
